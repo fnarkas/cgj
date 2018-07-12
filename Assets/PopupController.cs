b9c714e4bca4ffd611ae91e944d381190c6e8a78
Olle Landin <olle.landin@gmail.com>
2018-07-12 17:32:12 +0200

    Tweak: WIP

---
+++ Assets/PopupController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PopupController : MonoBehaviour {
+	public GameObject w;
+	public GameObject a;
+	public GameObject s;
+	public GameObject d;
+	public GameObject up;
+	public GameObject down;
+	public GameObject left;
+	public GameObject right;
+
+	public GameObject[] ghosts;
+
+	public void ShowGhost(int n){
+		int i =0;
+		foreach(var ghost in ghosts){
+			ghost.SetActive(false);
+			if(i < n)
+				ghost.SetActive(true);
+			i++;
+		}
+	}	
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}
