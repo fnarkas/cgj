1ef7d406919d00318f469bb9efa2efca80e9de59
Olle Landin <olle.landin@gmail.com>
2018-07-13 20:13:26 +0200

    Add: glitch effect

---
+++ Assets/CameraMaterialChanger.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMaterialChanger : MonoBehaviour {
+
+    public Material material; // material you want the camera to change
+    public bool Glitch;
+
+    void OnPreRender() {
+        if(Glitch)
+            material.SetInt("_IsOn",1);
+    }
+
+    void OnPostRender() {
+            material.SetInt("_IsOn",0);
+    }
+
+    private Color _default;
+}
